c2e069132b02528b9dae3a143c710391ad45344b
thepratholic <chelaramanipratham@gmail.com>
2026-09-07 09:51:58 +0530

    7/9/26

---
+++ LeetCode/Distinct Subsequences II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = (10 ** 9) + 7
+
+        dp = 1
+        last = [0] * 26
+
+        for ch in s:
+            idx = ord(ch) - ord('a')
+
+            new_dp = 2 * dp - last[idx]
+            new_dp %= MOD
+
+            last[idx] = dp
+            dp = new_dp
+
+        return (dp - 1) % MOD
